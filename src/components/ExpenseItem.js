d572b372c54cd496d5a3d78cee4d40311a8eb0d2
gustavohrg <gustavohrg@live.com>
2021-12-07 21:07:09 -0300

    C5: Passando valores dinâmicos no ExpenseItem

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css';
 
 function ExpenseItem() {
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = 'Seguro de carro';
+  const expenseAmount = 294.67;
+
   return (
     <div className='expense-item'>
-      <div>07 de dezembro de 2021</div>
+      <div>{expenseDate.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>Seguro de carro</h2>
-        <div className='expense-item__price'>R$ 294.67</div>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>R$ {expenseAmount}</div>
       </div>
     </div>
   );
