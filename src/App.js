afb5df7482d687004b9c37d74bce2baccbed9707
gustavohrg <gustavohrg@live.com>
2021-12-10 11:40:41 -0300

    C16: Bottom-Up Data ExpenseForm > NewExpense > App

--- src/App.js
+++ src/App.js
@@ -24,9 +24,14 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log(expense);
+    console.log('Updated expenses');
+  };
+
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,19 @@
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseDate) => {
+    const expenseData = {
+      ...enteredExpenseDate,
+      id: Math.random().toString(),
+    };
+
+    props.onAddExpense(expenseData);
+  };
+
   return (
     <div className='new-expense'>
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
     </div>
   );
 };
